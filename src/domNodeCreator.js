878ca6ec9e446ed61452da31147717ffc5bec1b4
bini-i <binyam.sh1@gmail.com>
2021-04-05 05:56:17 +0300

    Update index.js
    - add renderResult which renders fetched data from API

---
+++ src/domNodeCreator.js
@@ -0,0 +1,11 @@
+const domNodeCreator = (htmlTag, options = {}, text = '') => {
+  const node = document.createElement(htmlTag);
+  const keys = Object.keys(options);
+  keys.forEach((key) => {
+    node.setAttribute(key, options[key]);
+  });
+  node.textContent = text;
+  return node;
+};
+
+export { domNodeCreator };
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import {domNodeCreator} from './domNodeCreator'
+
 function construct_url(location, unit){
     return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=786b74dc8fce162462303a21c3e890c0&units=${unit}`
 }
@@ -8,7 +10,9 @@ function get_weather_data_for(location, unit){
             return response.json()
         })
         .then(function(data){
-            return data.main.temp
+            console.log('data fetched')
+            let temp = data.main.temp
+            render_result(temp, location, unit)            
         })
         .catch(function(err) {
             console.log(err)
@@ -17,13 +21,19 @@ function get_weather_data_for(location, unit){
 
 let result = document.getElementById('result')
 
-function show_result(){
-    
+function render_result(temp, location, unit){
+    console.log('rendering result')
+    const tempResult = document.createElement('p')
+    tempResult.textContent = temp
+    const locationResult = document.createElement('p')
+    locationResult.textContent = location
+    result.appendChild(tempResult)
+    result.appendChild(locationResult)
 }
 
 document.getElementById('submit').addEventListener('click', (e)=>{
     let location = document.getElementById('location').value
     let unit = document.getElementById('unit').value
-    let temp = get_weather_data_for(location, unit)
+    get_weather_data_for(location, unit)
 })
 
