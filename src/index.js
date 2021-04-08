dcab458c52e623e6b0526ebd9873441137648c22
bini-i <binyam.sh1@gmail.com>
2021-04-08 09:30:44 +0300

    Refactor index.js and add styling
    - centered content
    - added background

--- src/index.js
+++ src/index.js
@@ -1,30 +1,40 @@
-function constructurl(location, unit) {
+import domNodeCreator from './domNodeCreator';
+
+function constructURL(location, unit) {
   return `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=786b74dc8fce162462303a21c3e890c0&units=${unit}`;
 }
 
 const result = document.getElementById('result');
 
-function renderresult(temp, location) {
-  const tempResult = document.createElement('p');
-  tempResult.textContent = temp;
-  const locationResult = document.createElement('p');
-  locationResult.textContent = location;
-  result.appendChild(tempResult);
-  result.appendChild(locationResult);
+function renderResult(temp, location, icon, weatherDesc, unit) {
+  result.innerHTML = '';
+  const imgSrc = `http://openweathermap.org/img/wn/${icon}@4x.png`;
+  const locationResultEle = domNodeCreator('p', { class: 'location' }, location.toUpperCase());
+  const weatherIconEle = domNodeCreator('img', { src: imgSrc, alt: 'weather icon', class: 'weather-icon' });
+  const weatherDescEle = domNodeCreator('p', { class: 'weather-desc' }, weatherDesc);
+  const tempResultEle = domNodeCreator('p', { class: 'temp' }, temp);
+  const unitEle = domNodeCreator('sup', { class: 'unit' }, `${unit === 'metric' ? '°C' : '°F'}`);
+  result.appendChild(locationResultEle);
+  result.appendChild(weatherIconEle);
+  result.appendChild(weatherDescEle);
+  tempResultEle.appendChild(unitEle);
+  result.appendChild(tempResultEle);
 }
 
 function getweatherdatafor(location, unit) {
-  const url = constructurl(location, unit);
+  const url = constructURL(location, unit);
   fetch(url, { mode: 'cors' })
     .then((response) => response.json())
     .then((data) => {
       const { temp } = data.main;
-      renderresult(temp, location, unit);
+      const { icon } = data.weather[0];
+      const weatherDesc = data.weather[0].description;
+      renderResult(temp, location, icon, weatherDesc, unit);
     });
 }
 
 document.getElementById('submit').addEventListener('click', () => {
-  const location = document.getElementById('location').value;
-  const unit = document.getElementById('unit').value;
-  getweatherdatafor(location, unit);
+  const location = document.getElementById('city').value;
+  const unit = document.querySelector('.switch input').checked;
+  getweatherdatafor(location, (unit ? 'imperial' : 'metric'));
 });
